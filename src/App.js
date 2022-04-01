cebb8b198230e8e2096ea6cc25cb076f47356f5e
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-04-01 13:59:58 -0300

    Create: Carrinho (prob bug)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Carrinho from "./ComponentCarrinho/carrinho"
+import Itens from "./ComponentCarrinho/ProdutosCarrinho/produtos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    carrinho: [],
+    totalValue: 0,}
+
+  addCarrinho = (produto) => {
+    const produtoNoCarrinho = this.state.carrinho.filter((item) => {
+      if (item.id === produto.id) {
+        return item;
+      }else{
+        return false
+      }
+    });
+
+    if (produtoNoCarrinho.length === 0) {
+      produto.quantidade = 1;
+      const novoCarrinho = [produto, ...this.state.carrinho];
+      this.setState({
+        carrinho: novoCarrinho,
+      });
+    } else {
+      const novoCarrinho = this.state.carrinho.map((item) => {
+        if (produto.id === item.id) {
+          return { ...item, quantidade: item.quantidade + 1 };
+        } else {
+          return item;
+        }
+      });
+
+      this.setState({
+        carrinho: novoCarrinho,
+      });
+    }
+    this.addTotalValue(produto.preco);
+  };
+
+removeCarrinho = (itemParaRemover) => {
+    if (itemParaRemover.quantidade === 1) {
+      const novoCarrinho = this.state.carrinho.filter((item) => {
+        if (item.id !== itemParaRemover.id) {
+          return item;
+        }else{
+          return false
+        }
+      });
+      this.setState({
+        carrinho: novoCarrinho,
+      });
+    } else {
+      const novoCarrinho = this.state.carrinho.map((item) => {
+        if (itemParaRemover.id === item.id && item.quantidade >= 1) {
+          return { ...item, quantidade: item.quantidade - 1 };
+        } else {
+          return item;
+        }
+      });
+      this.setState({
+        carrinho: novoCarrinho,
+      });
+    }
+    this.removeTotalValue(itemParaRemover.preco)
+  };
+
+  addTotalValue = (valor) => {
+    this.setState({
+      totalValue: this.state.totalValue + valor
+    });
+  };
+
+  removeTotalValue = (valor) => {
+    this.setState({
+      totalValue: this.state.totalValue - valor
+    });
+  };
+
+
+  render() {
+    const produtosFiltrados = this.filtrarProdutos();
+    
+    return (
+      <div>
+        <Carrinho
+          carrinho={this.state.carrinho}
+          totalValue={this.state.totalValue}
+          removeCarrinho={this.removeCarrinho}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/ComponentCarrinho/carrinho.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { ConjuntoDoCarrinho } from "./carrinhoStyle";
+
+class Carrinho extends React.Component {
+  render() {
+    const itensDoCarrinho =
+      this.props.carrinho &&
+      this.props.carrinho.map((item) => {
+        return (
+          <Itens
+            key={item.id}
+            quantidade={item.quantidade}
+            nome={item.name}
+            onClick={() => this.props.removerItemDoCarrinho(item)}
+          />
+        );
+      });
+
+    return (
+      <ConjuntoDoCarrinho>
+        <h2>Carrinho:</h2>
+        <div>{itensDoCarrinho}</div>
+        <p>Valor total: R$ {this.props.valorTotal},00</p>
+      </ConjuntoDoCarrinho>
+    );
+  }}
+
+export default Carrinho;
