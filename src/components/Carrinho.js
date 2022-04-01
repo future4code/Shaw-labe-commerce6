c6af2a113713d724771e580d353fb50934c70437
Tácio Franco <98967235+Tryndamoron@users.noreply.github.com>
2022-04-01 14:18:14 -0300

    Carrinho (Com mais um bug, prob)

---
+++ src/components/Carrinho.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Itens from "./ProdutosCarrinho"
+import styled from "styled-components";
+
+const ConjuntoDoCarrinho = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    background-color: gray;
+    border-radius: 10px;
+    button {
+        border-radius: 5px;
+        background-color: #E0FFFF;
+        font-family: Roboto;
+        font-size: 15px;
+        border: 0px solid;
+        :hover {
+            background-color: #D3D3D3;
+        }
+    }
+`
+
+class Carrinho extends React.Component {
+  render() {
+    const itensDoCarrinho =
+      this.props.carrinho &&
+      this.props.carrinho.map((item) => {
+        return (
+          <Itens
+            key={item.id}
+            quantidade={item.quantidade}
+            nome={item.name}
+            onClick={() => this.props.removerItemDoCarrinho(item)}
+          />
+        );
+      });
+
+    return (
+      <ConjuntoDoCarrinho>
+        <h2>Carrinho:</h2>
+        <div>{itensDoCarrinho}</div>
+        <p>Valor total: R$ {this.props.valorTotal},00</p>
+      </ConjuntoDoCarrinho>
+    );
+  }}
+
+export default Carrinho;
---
+++ src/components/ProdutosCarrinho.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const ConjuntoDeItens = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  p {
+    margin: 10px 0;
+  }
+`;
+
+class Itens extends React.Component {
+    render() {
+      return (
+        <ConjuntoDeItens>
+          <p>{this.props.quantidade}x</p>
+          <p>{this.props.nome}</p>
+          <button onClick={this.props.onClick}>Remover</button>
+        </ConjuntoDeItens>
+      );
+    }
+  }
